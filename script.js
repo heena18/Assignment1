94f4c0ad680ef8c5004cebe10dbe860d8f74b899
Heena18 <heena.p.jain@gmail.com>
2015-09-17 17:27:34 -0700

    html&javascrift file

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+//step 1  
+
+/* 
+varsomeMonth;
+function theMonth		// function to return current month
+currentMonth 		// a constant
+var summerMonths; 		// an array of summer months
+MyLibrary-afunction	// a function
+*/
+
+
+
+//step2
+
+/* 
+var myvariable;
+myvariable = 3.9 				// numeric literal 
+myvariable = "Hello!" 			// string literal 
+myvariable = false 				// Boolean literal 
+myvariable = null 				// literal null value 
+*/
+
+
+
+//step3
+var anExpression = 3 * (4 / 5) + 6;
+var anexpression = 8 * 8
+
+
+
+//step4
+var firstName, lastName, Address, City, State, zipCode, yourAge, referralSource, mayWeContactYou;
+
+
+//step5
+
+var firstName = "heena", lastName = "jain", City = "san diego";
+
+
+//step6
+
+
